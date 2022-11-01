d8fe0bd88739e4b1b64cbfb62a87c81b593549d4
Leonardo Simas Rodrigues da Silva <leonardosimasrodrigues@gmail.com>
2022-11-01 16:17:19 -0300

    Inserção Ping para Teste AWS

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { app } from "./app"
 import { betsRouter } from "./router/betsRouter";
 import { groupsRouter } from "./router/groupsRouter";
 import { matchesRouter } from "./router/matchesRouter";
+import { pingRouter } from "./router/pingRouter";
 import { userRouter } from "./router/userRouter";
 
 //Chamada para os routers
@@ -9,6 +10,7 @@ app.use("/matches", matchesRouter);
 app.use("/user", userRouter);
 app.use("/groups", groupsRouter)
 app.use("/bets" , betsRouter)
+app.use("/ping" , pingRouter)
 
 
 
